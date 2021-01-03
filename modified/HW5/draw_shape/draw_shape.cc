03df7e29e6cd368c7aec09f5b09f760939858c99
vinnyshin <tankmom7279@gmail.com>
2021-01-04 00:11:46 +0900

    draw_shape AddShape, DeleteShape complete

--- modified/HW5/draw_shape/draw_shape.cc
+++ modified/HW5/draw_shape/draw_shape.cc
@@ -1,7 +1,8 @@
 #include "draw_shape.h"
 
 Canvas::Canvas(size_t row, size_t col) {
-
+    row_ = row;
+    col_ = col;
 }
 
 Canvas::~Canvas() {
@@ -9,15 +10,17 @@ Canvas::~Canvas() {
 }
 
 int Canvas::AddShape(const Shape &s) {
-
+    shapes_.push_back(s);
+    // Return the index of the shape.
+    return shapes_.size() - 1;
 }
 
 void Canvas::DeleteShape(int index) {
-
+    shapes_.erase(shapes_.begin() + index);
 }
 
 void Canvas::Draw(ostream &os) {
-
+    
 }
 
 void Canvas::Dump(ostream &os) {
--- modified/HW5/draw_shape/draw_shape_main.cc
+++ modified/HW5/draw_shape/draw_shape_main.cc
@@ -3,6 +3,34 @@
 
 int main(int argc, char const *argv[])
 {
+    size_t row, column;
     
+    cin >> column >> row;
+    cin.ignore();
+
+    string command;
+
+    while (true)
+    {
+        getline(cin, command);
+        if (command.find("add") != string::npos) {
+            cout << "add" << endl;
+        }
+        else if (command.find("draw") != string::npos) {
+            cout << "draw" << endl;
+        }
+        else if (command.find("dump") != string::npos) {
+            cout << "dump" << endl;
+        }
+        else if (command.find("delete") != string::npos) {
+            cout << "delete" << endl;
+        }
+        else if (command.find("quit") != string::npos) {
+            cout << "quit" << endl;
+        }
+        else {
+            cout << "invalid input" << endl;
+        }
+    }
     return 0;
 }
