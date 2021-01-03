e46efa813e1b80fffa85d7f638ef74521a126c47
root <root@DESKTOP-7PMNGV8.localdomain>
2021-01-03 21:44:47 +0900

    draw_shape HW5 structure made

---
+++ modified/HW5/draw_shape/draw_shape.cc
@@ -0,0 +1,25 @@
+#include "draw_shape.h"
+
+Canvas::Canvas(size_t row, size_t col) {
+
+}
+
+Canvas::~Canvas() {
+
+}
+
+int Canvas::AddShape(const Shape &s) {
+
+}
+
+void Canvas::DeleteShape(int index) {
+
+}
+
+void Canvas::Draw(ostream &os) {
+
+}
+
+void Canvas::Dump(ostream &os) {
+
+}
---
+++ modified/HW5/draw_shape/draw_shape_main.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "draw_shape.h"
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
