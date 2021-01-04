b7b585e546564f1e7ed9dcd1ecfe3f729c6a3daf
vinnyshin <tankmom7279@gmail.com>
2021-01-04 19:15:09 +0900

    draw_shape_main complete

--- modified/HW5/draw_shape/draw_shape.cc
+++ modified/HW5/draw_shape/draw_shape.cc
@@ -16,13 +16,46 @@ int Canvas::AddShape(const Shape &s) {
 }
 
 void Canvas::DeleteShape(int index) {
+    if (index >= shapes_.size())
+        return;
     shapes_.erase(shapes_.begin() + index);
 }
 
 void Canvas::Draw(ostream &os) {
+    vector<vector<char> > map;
+    map.resize(row_, vector<char>(col_, '.'));
     
+    /* 
+    Making map
+    Ex)
+    01234.....column
+    1.........
+    2.........
+    3.........
+    4.........
+    .
+    .
+    .
+    row
+    */
+    
+    for (size_t i = 0; i < row_; i++)
+        map[i][0] = i;
+    
+    for (size_t i = 0; i < col_; i++)
+        map[0][i] = i;
+
 }
 
 void Canvas::Dump(ostream &os) {
-
+    for (size_t i = 0; i < shapes_.size(); i++)
+    {
+        cout << i << " " << 
+        shapes_[i].type <<
+        shapes_[i].x <<
+        shapes_[i].y <<
+        shapes_[i].width <<
+        shapes_[i].height <<
+        shapes_[i].brush << endl;
+    }
 }
--- modified/HW5/draw_shape/draw_shape_main.cc
+++ modified/HW5/draw_shape/draw_shape_main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bits/stdc++.h>
 #include "draw_shape.h"
 
 int main(int argc, char const *argv[])
@@ -8,26 +9,105 @@ int main(int argc, char const *argv[])
     cin >> column >> row;
     cin.ignore();
 
+    Canvas cv(row, column);
+
     string command;
 
     while (true)
     {
         getline(cin, command);
         if (command.find("add") != string::npos) {
-            cout << "add" << endl;
+            //Makig "add rect x y height width brush" to "rect x y height width brush"
+            command.erase(0, 4);
+            
+            Shape shape;
+            vector<string> vs;
+            istringstream iss(command);
+            string token;
+            
+            while (getline(iss, token, ' '))
+                vs.push_back(token);
+            
+            //다시... rect 는 중심값 기준으로... 
+            //x,y 좌표는 말그대로 좌표값 기준 index값 기준이 아님!
+            if (vs[0] == "rect") {
+                shape.type = RECTANGLE;
+                shape.x = stoi(vs[1]);
+                shape.y = stoi(vs[2]);
+                shape.width = stoi(vs[3]);
+                shape.height = stoi(vs[4]);
+                shape.brush = vs[5][0];
+
+                if(shape.x + shape.width > column
+                || shape.y + shape.height > row  
+                || shape.x > column 
+                || shape.y > row ) {
+                    cout <<"error out of canvas" << endl;
+                    continue;
+                }
+            }
+
+            else if (vs[0] == "tri_up") {
+                shape.type = TRIANGLE_UP;
+                shape.x = stoi(vs[1]);
+                shape.y = stoi(vs[2]);
+                shape.height = stoi(vs[3]);
+                shape.brush = vs[4][0];
+
+                
+                //shape.x + shape.height - 1
+                /*
+                        --- << length == 3 == x + height - 1
+                        #..      < 
+                       ###.      <  ->  height == 3
+                      #####      <
+
+                */
+                if(shape.x + shape.height - 1 > column
+                || shape.y + shape.height > row  
+                || shape.x > column 
+                || shape.y > row ) {
+                    cout <<"error out of canvas" << endl;
+                    continue;
+                }
+            }
+
+            else if (vs[0] == "tri_down") {
+                shape.type = TRIANGLE_DOWN;
+                shape.x = stoi(vs[1]);
+                shape.y = stoi(vs[2]);
+                shape.height = stoi(vs[3]);
+                shape.brush = vs[4][0];
+
+                if(shape.x + shape.height - 1 > column
+                || shape.y - shape.height < 1  
+                || shape.x > column 
+                || shape.y > row ) {
+                    cout <<"error out of canvas" << endl;
+                    continue;
+                }
+            }
+            cv.AddShape(shape);
         }
+
         else if (command.find("draw") != string::npos) {
-            cout << "draw" << endl;
+            cv.Draw(cout);
         }
+
         else if (command.find("dump") != string::npos) {
-            cout << "dump" << endl;
+            cv.Dump(cout);
         }
+
         else if (command.find("delete") != string::npos) {
-            cout << "delete" << endl;
+            command.erase(0, 7);
+            int index = stoi(command);
+            cv.DeleteShape(index);
         }
+
         else if (command.find("quit") != string::npos) {
-            cout << "quit" << endl;
+            break;
         }
+
         else {
             cout << "invalid input" << endl;
         }
