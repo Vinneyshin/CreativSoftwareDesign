4178ae4ccf6e9da960bfc219a2fd602aebb5caa9
vinnyshin <tankmom7279@gmail.com>
2021-01-09 16:52:06 +0900

    radix_notation complete

--- modified/HW2/radix_notation.cc
+++ modified/HW2/radix_notation.cc
@@ -7,7 +7,29 @@ using namespace std;
 /* Implement this function. */
 
 string RadixNotation(unsigned int number, unsigned int radix) {
+        unsigned int diviend = number, divisor = radix;
+        string temp = "";
         
+        char radixTable[36];
+
+        for (size_t i = 0; i < 10; i++)
+        {
+            radixTable[i] = '0' + i;
+        }
+
+        for (size_t i = 10; i < 36; i++)
+        {
+            radixTable[i] = 'a' + (i - 10);
+        }
+        
+        while (diviend != 0)
+        {
+            temp = radixTable[diviend % divisor] + temp;
+            diviend = diviend / divisor;
+        }
+        
+        // quotient + remainder;
+        return temp; 
 }
 
 int main(int argc, char **argv)
