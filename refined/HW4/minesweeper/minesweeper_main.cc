74764090a518d4ed2c99b0fc577dca999e4ea3bd
vinnyshin <tankmom7279@gmail.com>
2021-01-10 21:27:48 +0900

    implement minesweeper_main.cc

--- refined/HW4/minesweeper/minesweeper_main.cc
+++ refined/HW4/minesweeper/minesweeper_main.cc
@@ -1,21 +1,80 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include "minesweeper.h"
 
 using namespace std;
 
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
-    int row, column;
+    size_t row, column;
+    string command;
+    Minesweeper minesweeper;
 
-    cin >> column >> row;
+    while (true)
+    {
+        getline(cin, command);
 
+        if (command.find(":set") != string::npos)
+        {
+            command.erase(0, 4);
+            // After erasing this command would be like "%d %d"
+            istringstream iss(command);
 
-    Minesweeper ms(row, column);
+            iss >> column >> row;
 
-    ms.setMap();
+            minesweeper = Minesweeper(row, column);
+            minesweeper.setMap();
+            minesweeper.calMap();
+        }
+        else if (command.find(":toggle") != string::npos)
+        {
+            command.erase(0, 7);
 
+            istringstream iss(command);
+            // x stands for column and y stands for row.
+            size_t x, y;
 
+            iss >> x >> y;
+
+            minesweeper.toggleMine(x, y);
+            minesweeper.calMap();
+        }
+        else if (command.find(":play") != string::npos)
+        {
+            bool isDead = false;
+            size_t x, y;
+            istringstream iss;
+
+            while (!isDead)
+            {
+                minesweeper.setPlay();
+                getline(cin, command);
+
+                if (command.find(":touch") != string::npos)
+                {
+                    command.erase(0, 6);
+                    iss = istringstream(command);
+                    iss >> x >> y;
+
+                    isDead = minesweeper.touchMap(x, y);
+                }
+                else
+                {
+                    cout << "invalid input" << endl;
+                }
+            }
+            cout << "DEAD(" << minesweeper.touchCount() << ")" << endl;
+        }
+        else if (command.find(":quit"))
+        {
+            break;
+        }
+        else
+        {
+            cout << "invalid input" << endl;
+        }
+    }
 
     return 0;
 }
